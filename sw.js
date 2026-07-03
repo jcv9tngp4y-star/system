aa3dec5c05ff0f413f6e7966088bbcaaa81be3fc
jcv9tngp4y-star <jcv9tngp4y@privaterelay.appleid.com>
2026-07-03 06:26:00 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+/* THE SYSTEM - offline cache */
+const CACHE = "system-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-180.png", "./icon-512.png"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(hit => hit ||
+      fetch(e.request).then(r => {
+        const copy = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return r;
+      }).catch(() => caches.match("./index.html"))
+    )
+  );
+});
